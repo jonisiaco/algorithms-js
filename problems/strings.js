a724c11927c38d29d8ad4c050fbe31eb294c25b6
Jon Rodriguez <jonisiaco@Jons-MacBook-Pro-2.local>
2025-06-07 01:25:21 -0600

    string problems

--- problems/strings.js
+++ problems/strings.js
@@ -15,5 +15,24 @@ function reverseString(s) {
   return chars.join('');
 }
 
-// Example usage:
+// Two-Pointer Technique
 console.log(reverseString("hello")); // Output: "olleh"
+
+function isPalindrome(s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
+
+// Example usage:
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("hello"));   // false
