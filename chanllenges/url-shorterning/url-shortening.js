6b714f5c5694ebf5fab5d9b333acaa75e429084e
Jon Rodriguez <jonisiaco@Jons-MacBook-Pro-2.local>
2025-06-09 00:50:34 -0600

    challenges

---
+++ chanllenges/url-shorterning/url-shortening.js
@@ -0,0 +1,14 @@
+const json = require('./url-shortening.json');
+
+function minify(json, entry) {
+    json.forEach(data => {
+        const originalUrl = data.original_url;
+        const redirectUrl = data.short_url;
+        if (originalUrl === entry) {
+            console.log('redirecting: ', redirectUrl);
+        }
+    });
+
+}
+const url = 'https://www.example.com/articles/how-to-grow-tomatoes-in-winter';
+minify(json, url);
