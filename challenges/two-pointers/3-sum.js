688b826fb34352a2ea81e537f07920dd4c798262
Jon Rodriguez <jonisiaco@Jons-MacBook-Pro-2.local>
2025-06-16 20:57:33 -0600

    two sum

---
+++ challenges/two-pointers/3-sum.js
@@ -0,0 +1,6 @@
+/*
+    Given an input integer array nums, write a function to find all unique triplets 
+    [nums[i], nums[j], nums[k]] such that i, j, and k are distinct indices, and the sum of 
+    nums[i], nums[j], and nums[k] equals zero. Ensure that the resulting list does not contain any duplicate triplets.
+*/
+
--- challenges/two-pointers/two-sum-pairs.js
+++ challenges/two-pointers/two-sum-pairs.js
@@ -9,15 +9,18 @@ function isPairSum(nums, target) {
     return false;
 }
 
-function twoSum(nums, target) {
+function twoSum(numbers, target) {
+    const nums = numbers.map((n, idx) => [n,idx]);
+    nums.sort((a,b) => a[0] - b[0]);
+    console.log(nums)
     let left = 0;
     let right = nums.length - 1;
 
     while (left < right) {
-        let currentSum = nums[left] + nums[right];
+        let currentSum = nums[left][0] + nums[right][0];
 
         if (currentSum === target) {
-            return true;
+            return [ nums[left][1] , nums[right][1] ];
         }
 
         if (currentSum < target) {
@@ -27,8 +30,14 @@ function twoSum(nums, target) {
         }
     }
 
-    return false;
+    return [];
 }
+const nums = [7, 2, 5, 4, 1, 9, 8];
+const target = 10;
+
+const result = twoSum(nums, target);
+console.log("Result: ", result);
+
 
 // The function hasPairWithSum(arr, target) checks whether there exists a pair of numbers
 //  in the given array arr that adds up to the specified target sum. It returns true if such a pair exists,
